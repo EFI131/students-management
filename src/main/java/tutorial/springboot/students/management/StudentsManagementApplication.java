5006f7949a946dc83baa9d897b4c3ac881987c5d
EFI131 <efi131@gmail.com>
2021-02-09 00:35:20 +0800

    initial commit

---
+++ src/main/java/tutorial/springboot/students/management/StudentsManagementApplication.java
@@ -0,0 +1,13 @@
+package tutorial.springboot.students.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsManagementApplication.class, args);
+	}
+
+}
